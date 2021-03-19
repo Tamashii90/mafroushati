2473213ade41e36639a1614eb85208a1681ac229
pato-boy <patoboy90@hotmail.com>
2021-03-19 15:44:10 +0200

    fix image bug on manual url navigation

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -48,7 +48,7 @@ export default function Header() {
       <nav className="navbar navbar-expand-xl navbar-light pb-5 pb-xl-3">
         <div className="navbar-start my-3">
           <Link className="navbar-brand mr-0" to="/">
-            <img src="images/logo.png" alt="Mafroushati" />
+            <img src="/images/logo.png" alt="Mafroushati" />
           </Link>
           <button
             className="navbar-toggler"
