e6bdf7f8b239d45ed941c8827be3dab5f412e32c
Tamashii90 <patoboy90@hotmail.com>
2022-01-12 15:14:37 +0200

    adjust placeholder style

--- frontend/src/components/Placeholder/ProductCard.jsx
+++ frontend/src/components/Placeholder/ProductCard.jsx
@@ -1,21 +1,20 @@
 import React from "react";
-import { TextBlock, RectShape } from "react-placeholder/lib/placeholders";
+import { RectShape } from "react-placeholder/lib/placeholders";
 
 export default function Placeholder() {
-  return (
-    <div className="col-12 my-3 placeholder-animated">
-      <div className="text-center justify-content-center">
-        <RectShape
-          color="#E0E0E0"
-          className="col-12"
-          style={{
-            display: "inline-block",
-            height: "300px",
-            marginRight: 0
-          }}
-        />
-        <TextBlock color="#E0E0E0" rows={4} className="py-4" />
-      </div>
-    </div>
-  );
+	return (
+		<div className="col-12 my-3 placeholder-animated">
+			<div className="text-center justify-content-center">
+				<RectShape
+					color="#E0E0E0"
+					className="col-12"
+					style={{
+						display: "inline-block",
+						height: "300px",
+						marginRight: 0
+					}}
+				/>
+			</div>
+		</div>
+	);
 }
