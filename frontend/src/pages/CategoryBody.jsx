65547dc1d8ecde6ec12477e40a0a921904a02a44
Tamashii90 <patoboy90@hotmail.com>
2022-01-15 22:48:41 +0200

    fix bug causing slider to re-render on value-change

--- frontend/src/pages/CategoryBody.jsx
+++ frontend/src/pages/CategoryBody.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState, useRef } from "react";
+import React, { useEffect, useState } from "react";
 import ProductGallery from "../components/ProdsGallery";
 import { useParams, useLocation } from "react-router-dom";
 import { fetcher } from "../utils";
@@ -13,37 +13,38 @@ import { toast } from "react-toastify";
 export default function CategoryPage() {
 	const { pathname } = useLocation();
 	const [filter, setFilter] = useState({ min: 0, max: Infinity });
-	const [price, setPrice] = useState({ min: null, max: null });
 	const [skip, setSkip] = useState(0);
-	const prodPerPage = 9;
 	const [count, setCount] = useState();
+	const [price, setPrice] = useState();
+	const prodPerPage = 9;
 	const { category } = useParams();
-
-	const { data, error } = useSWR(
-		`/api/category/${category}?min=${filter.min}&max=${filter.max}&skip=${skip}`,
-		fetcher
-	);
-	useEffect(() => {
-		setSkip(0);
-		setCount();
-		setFilter({ min: 0, max: Infinity });
-		setPrice({ min: null, max: null });
-	}, [pathname]);
-	const products = data?.products;
-	if (products?.length && !price.min) {
-		// only update the price and count once (when initial value is null)
-		setCount(data.count);
-		setPrice({ min: data.min, max: data.max });
-	}
-	const filterByPrice = (e, value) => {
-		setFilter({ min: value[0], max: value[1] });
-	};
 	const categoryName = category
 		.split("+")
 		.map(word => word[0].toUpperCase() + word.slice(1))
 		.join(" ")
 		.replace("Tv", "TV");
+	const filterByPrice = (e, value) => setFilter({ min: value[0], max: value[1] });
+
+	useEffect(() => {
+		setCount();
+		setPrice();
+		setSkip(0);
+		setFilter({ min: 0, max: Infinity });
+	}, [pathname]);
+
+	const { data, error } = useSWR(
+		`/api/category/${category}?min=${filter.min}&max=${filter.max}&skip=${skip}`,
+		fetcher
+	);
 	if (error) toast.error(error.message);
+	if (data && !count) {
+		// only update the price and count once (when they are still undefined)
+		setCount(data.count);
+		setPrice({ min: data.min, max: data.max });
+	}
+
+	const products = data?.products;
+
 	return (
 		<div className="container">
 			<Helmet>
@@ -51,24 +52,19 @@ export default function CategoryPage() {
 			</Helmet>
 			<h1 className="text-center">{categoryName}</h1>
 			<div className="row justify-content-center justify-content-md-start">
-				{products ? (
-					<>
-						<Slider
-							className="col-10 mx-auto"
-							onChangeCommitted={filterByPrice}
-							minPrice={price.min}
-							maxPrice={price.max}
-							valueLabelDisplay="auto"
-						/>
-						<ProductGallery products={products} />
-					</>
+				{price ? (
+					<Slider
+						className="col-10 mx-auto"
+						onChangeCommitted={filterByPrice}
+						minPrice={price.min}
+						maxPrice={price.max}
+						valueLabelDisplay="auto"
+					/>
 				) : (
-					<>
-						<SliderPlaceholder />
-						<Placeholder />
-					</>
+					<SliderPlaceholder />
 				)}
-				{!!products?.length && (
+				{products ? <ProductGallery products={products} /> : <Placeholder />}
+				{products?.length > 0 && (
 					<Pager
 						pages={Math.ceil(count / prodPerPage)}
 						setSkip={setSkip}
