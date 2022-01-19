9a18e1e8a0c96eadf54551a488af2fd44bd8f8ad
Tamashii90 <patoboy90@hotmail.com>
2022-01-19 22:13:30 +0200

    edit about page

--- frontend/src/pages/AboutBody.jsx
+++ frontend/src/pages/AboutBody.jsx
@@ -3,14 +3,14 @@ import React from "react";
 export default function AboutBody() {
 	return (
 		<div className="container text-center">
+			<h2>Notice</h2>
 			<p>
 				<strong>This website isn't real.</strong> It was made for educational purposes.
 				<br />
-				Despite what the footer says,&nbsp;
-				<strong>Mafroushati isn't a real company.</strong>
+				Despite what the footer says, Mafroushati&nbsp;
+				<strong>isn't a real company.</strong>
 			</p>
-			<h2 className="mt-5">Credits</h2>
-			<ul className="text-left mt-3">
+			<ul className="text-left mt-5">
 				<li>
 					The logo was made with the help of&nbsp;
 					<a href="https://www.freelogodesign.org/" target="_blank">
@@ -19,13 +19,13 @@ export default function AboutBody() {
 					.
 				</li>
 				<li className="mt-2">
-					Images taken from&nbsp;
-					<a href="https://hipwallpaper.com" target="_blank">
-						hipwallpaper.com
-					</a>
-					&nbsp;and&nbsp;
+					Images were taken from&nbsp;
 					<a href="https://unsplash.com/" target="_blank">
 						unsplash.com
+					</a>
+					&nbsp;and&nbsp;
+					<a href="https://hipwallpaper.com" target="_blank">
+						hipwallpaper.com
 					</a>
 					.
 				</li>
